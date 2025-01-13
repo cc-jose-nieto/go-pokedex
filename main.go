8f4598a53b456d2fbb82ecb1f42fa92c85841922
Jose G Nieto <jose.nieto@noblenex.us>
2025-01-13 13:47:41 -0500

    Ch1-L4 REPL

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	scanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
+	for {
+		fmt.Print("Pokedex > ")
+		input := scanner.Scan()
+		if !input {
+			break
+		}
+		words := cleanInput(scanner.Text())
+		if len(words) > 0 {
+			fmt.Printf("Your command was: %s\n", words[0])
+		}
+	}
 }
 
 func cleanInput(text string) []string {
