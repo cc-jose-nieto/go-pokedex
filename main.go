0e17ad92ac75e7b66c6bc23447f10e1084fa0990
Jose G Nieto <jose.nieto@noblenex.us>
2025-01-13 11:21:28 -0500

    Ch1-L3 REPL

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Hello, world!")
+}
+
+func cleanInput(text string) []string {
+	text = strings.ToLower(text)
+	text = strings.TrimSpace(text)
+	words := strings.Split(text, " ")
+	var newWords []string
+	for i := range words {
+		if strings.TrimSpace(words[i]) != "" {
+			newWords = append(newWords, words[i])
+		}
+	}
+	return newWords
 }
